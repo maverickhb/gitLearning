feafe4d0bdaf1242343e27f0a45167cdb108f315
maverickhb <hasbeker@gmail.com>
2020-05-07 22:14:20 +0300

    add Winter_V1.java

---
+++ src/tests/Winter_V1.java
@@ -0,0 +1,9 @@
+package tests;
+
+public class Winter_V1 {
+    public static void main(String[] args) {
+        System.out.println("This is winter class");
+        System.out.println("Use git menu commit");
+        System.out.println("Adding something for version control");
+    }
+}
