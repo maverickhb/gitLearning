ba4f8a37d783f543697de43c212444f5e18010fa
maverickhb <hasbeker@gmail.com>
2020-05-07 22:05:24 +0300

    update Winter.java

--- src/tests/Winter.java
+++ src/tests/Winter.java
@@ -4,5 +4,6 @@ public class Winter {
 
     public static void main(String[] args) {
         System.out.println("This is winter class");
+        System.out.println("Use git menu commit");
     }
 }
