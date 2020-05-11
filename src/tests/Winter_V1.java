0d7c9bd7be29d84decee808b204af17bf6e79a9d
maverickhb <hasbeker@gmail.com>
2020-05-11 15:59:06 +0300

    update Winter_V1.java

--- src/tests/Winter_V1.java
+++ src/tests/Winter_V1.java
@@ -6,6 +6,8 @@ public class Winter_V1 {
         System.out.println("Use git menu commit");
         System.out.println("Adding something for version control");
         System.out.println("test just test");
+        System.out.println("Test vcs commit and push");
+
 
     }
 }
